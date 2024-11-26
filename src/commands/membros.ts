5e0e4d7360115b5f71ee3ac1877d1466fa8da359
João Pedro <joaopedro@sobremail.com>
2024-11-26 20:56:39 -0300

    overwriting local CSV file with the user CSV

--- src/commands/membros.ts
+++ src/commands/membros.ts
@@ -1,4 +1,4 @@
-import { readFileSync, writeFileSync } from "fs"
+import { readFileSync, writeFileSync, writeFile } from "fs"
 import path from "path";
 import { Readable } from "stream";
 import { Message, MessageMedia } from "whatsapp-web.js";
@@ -15,17 +15,17 @@ export default async (msg: Message) => {
 
     const receivedFile = await msg.downloadMedia()
     const chat = await msg.getChat()
+    const csvPath = path.join(__dirname, '../public/members.csv');
 
     if(!receivedFile){
-        const exampleCsvPath = path.join(__dirname, '../public/members-example.csv');
-        const base64ExampleCsv = readFileSync(exampleCsvPath, 'base64');
+        const base64Csv = readFileSync(csvPath, 'base64');
         
         // Crie a mídia que será enviada como resposta
-        const media = new MessageMedia('text/csv', base64ExampleCsv, 'Planilha-de-exemplo.csv')
+        const media = new MessageMedia('text/csv', base64Csv, 'Planilha-de-exemplo.csv')
         
         // Responder à mensagem com o arquivo CSV de exemplo anexado
         return await msg.reply(
-            'Você precisa anexar uma planilha CSV para poder adicionar membros. Aqui vai uma planilha exemplo para você preencher:',
+            'Anexe uma planilha CSV para poder alterar os membros. Esta é a planilha dos membros atuais. Baixe-á, edite-á e anexe-á utilizando este comando (membros)',
             chat.id._serialized,
             { media }
         );
@@ -53,15 +53,26 @@ export default async (msg: Message) => {
                 cleaningDay: result['dia de limpeza']?? undefined,
             }))
 
+            writeFile(csvPath, csvBuffer, (err) => {
+                if (err) {
+                    console.error('Erro ao substituir o arquivo CSV:', err);
+                    return msg.reply('Erro ao editar a planilha de membros')
+                } else {
+                    console.log('Arquivo CSV substituído com sucesso!');
+                }
+            });
+
             const configPath = path.resolve(__dirname, '../public/config.json');
             const config = readFileSync(configPath, 'utf-8')
             const parsedConfig = JSON.parse(config)
         
-            const oldMembers: Member[] = parsedConfig.members
+            const oldMembers: Member[] = parsedConfig.members?? []
             const allMembers = [...oldMembers, ...members]
             parsedConfig.members = allMembers
         
-            const newConfig = JSON.stringify(parsedConfig, null, '/t')
+            const newConfig = JSON.stringify(parsedConfig, null, 4)
             writeFileSync(configPath, newConfig)
         });
+
+        await msg.reply('Membros alterados com sucesso!')
 }
