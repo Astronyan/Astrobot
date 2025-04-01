d9f1efc524fced82fee948e1a02cb9bc7e23c104
João Pedro <joaopedro@sobremail.com>
2025-04-01 20:44:50 -0300

    fix error handler on transcrever

--- src/commands/transcrever.ts
+++ src/commands/transcrever.ts
@@ -4,24 +4,23 @@ import base64toFile from "../lib/transcribe/base64toFile";
 
 export default async (msg: Message, ...params: any) => {
 
-    
-    if (!msg.hasQuotedMsg) return await msg.reply('Você precisa citar o audio no qual quer transcrever. Você não citou nenhum mensagem')
+    try {
+        if (!msg.hasQuotedMsg) return await msg.reply('Você precisa citar o audio no qual quer transcrever. Você não citou nenhum mensagem')
 
-    const quotedMessage = await msg.getQuotedMessage()
-    const audioMedia = await quotedMessage.downloadMedia()
-    if (!quotedMessage.hasMedia || !audioMedia.mimetype || !audioMedia.mimetype.startsWith('audio/ogg'))
-     return await msg.reply('A mensagem que você mencionou não possui nenhum audio anexado')
+        const quotedMessage = await msg.getQuotedMessage()
+        const audioMedia = await quotedMessage.downloadMedia()
+        if (!quotedMessage.hasMedia || !audioMedia.mimetype || !audioMedia.mimetype.startsWith('audio/ogg'))
+            return await msg.reply('A mensagem que você mencionou não possui nenhum audio anexado')
 
-    const chat = await msg.getChat()
-    chat.sendStateTyping()
+        const chat = await msg.getChat()
+        chat.sendStateTyping()
 
-    const audioBase64 = audioMedia.data
-    const audioFile = base64toFile(audioBase64)
+        const audioBase64 = audioMedia.data
+        const audioFile = base64toFile(audioBase64)
 
-    try{
         const transcribedAudio = await whisper(audioFile)
         msg.reply(transcribedAudio)
-    } catch(err){
+    } catch (err) {
         console.error(err)
         msg.reply('Houve um erro ao transcrever a sua mensagem')
     }
